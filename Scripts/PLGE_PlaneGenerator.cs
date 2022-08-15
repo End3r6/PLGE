1ebc3d26133a5990cb980c2821f53f3c4552e46d
End3r6 <jswardson@cnm.edu>
2022-08-15 16:04:50 -0600

    Logs warning if mesh is to large for unity to create

--- Scripts/PLGE_PlaneGenerator.cs
+++ Scripts/PLGE_PlaneGenerator.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
+using System;
+
 using UnityEngine;
 
 [RequireComponent(typeof(MeshRenderer))]
@@ -21,43 +24,51 @@ public class PLGE_PlaneGenerator : MonoBehaviour
 
     public void GenerateMesh()
     {
-        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
-		mesh.name = "PLGE_Plane";
-
-        vertices = new Vector3[(numberOfFaces.x + 1) * (numberOfFaces.y + 1) * (meshResolution * meshResolution)];
-        Vector2[] uv = new Vector2[vertices.Length];
-
-        Vector4[] tangents = new Vector4[vertices.Length];
-		Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
-		for (int i = 0, z = 0; z <= meshResolution; z++) 
+        try
         {
-			for (int x = 0; x <= meshResolution; x++, i++) 
+            GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+            mesh.name = "PLGE_Plane";
+
+            vertices = new Vector3[(numberOfFaces.x + 1) * (numberOfFaces.y + 1) * (meshResolution * meshResolution)];
+            Vector2[] uv = new Vector2[vertices.Length];
+
+            Vector4[] tangents = new Vector4[vertices.Length];
+            Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+            for (int i = 0, z = 0; z <= meshResolution; z++) 
             {
-				vertices[i] = (new Vector3(x * numberOfFaces.x, 0, z * numberOfFaces.y) / meshResolution) - new Vector3(numberOfFaces.x / 2, 0, numberOfFaces.y / 2);
-                uv[i] = new Vector2((float)x, (float)z) / meshResolution;
+                for (int x = 0; x <= meshResolution; x++, i++) 
+                {
+                    vertices[i] = (new Vector3(x * numberOfFaces.x, 0, z * numberOfFaces.y) / meshResolution) - new Vector3(numberOfFaces.x / 2, 0, numberOfFaces.y / 2);
+                    uv[i] = new Vector2((float)x, (float)z) / meshResolution;
 
-                tangents[i] = tangent;
-			}
-		}
+                    tangents[i] = tangent;
+                }
+            }
 
-        int[] triangles = new int[meshResolution * meshResolution * 6];
-		for (int ti = 0, vi = 0, y = 0; y < meshResolution; y++, vi++) 
+            int[] triangles = new int[meshResolution * meshResolution * 6];
+            for (int ti = 0, vi = 0, y = 0; y < meshResolution; y++, vi++) 
+            {
+                for (int x = 0; x < meshResolution; x++, ti += 6, vi++) 
+                {
+                    triangles[ti] = vi;
+                    triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                    triangles[ti + 4] = triangles[ti + 1] = vi + meshResolution + 1;
+                    triangles[ti + 5] = vi + meshResolution + 2;
+                }
+            }
+
+            // Set the current mesh filter to use our generated mesh
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+            mesh.uv = uv;
+            mesh.tangents = tangents;
+
+            mesh.RecalculateNormals();
+        }
+        catch(Exception e)
         {
-			for (int x = 0; x < meshResolution; x++, ti += 6, vi++) 
-            {
-				triangles[ti] = vi;
-				triangles[ti + 3] = triangles[ti + 2] = vi + 1;
-				triangles[ti + 4] = triangles[ti + 1] = vi + meshResolution + 1;
-				triangles[ti + 5] = vi + meshResolution + 2;
-			}
-		}
-
-        // Set the current mesh filter to use our generated mesh
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-        mesh.uv = uv;
-        mesh.tangents = tangents;
-
-        mesh.RecalculateNormals();
+            Debug.LogWarning("You are trying to create a mesh with more verticies than Unity allows on a single mesh\n"
+            + "Consider spliting the mesh into chunks or changing the mesh index");
+        }
     }
 }
